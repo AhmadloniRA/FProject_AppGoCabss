54e401e5f0b5a0abf46d79e0f1f78883fd2c581f
AhmadloniRA <if23.ahmadloniamin@mhs.ubpkarawang.ac.id>
2025-06-14 11:43:14 +0700

    membuat tampilan activity untuk logikanya belum menyusul

--- src/app/activity/activity.page.ts
+++ src/app/activity/activity.page.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityPage implements OnInit {
 
-  constructor() {}
+  selectedTab = 'riwayat';
 
-  ngOnInit() {
-  }
+  constructor() { }
+
+  ngOnInit() { }
+
 }
