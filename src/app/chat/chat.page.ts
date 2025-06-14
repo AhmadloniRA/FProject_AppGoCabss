d08e45acafa9fec3d7f4a7cdc034bff9e6af5a35
AhmadloniRA <if23.ahmadloniamin@mhs.ubpkarawang.ac.id>
2025-06-14 10:32:23 +0700

    (memindahkan code ke page aslinya) memindahkan code dari page activity ke page payment dan code di page chat ke page rating

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   standalone: false,
@@ -8,59 +7,9 @@ import { NavController, AlertController } from '@ionic/angular';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-  rating = 0;
-  stars = new Array(5);
-  selected: string = '';
 
-  constructor(
-    private navCtrl: NavController,
-    private alertController: AlertController
-  ) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.selected = ''; // Reset saat awal component dibuka pertama kali
-  }
+  ngOnInit() {}
 
-  ionViewWillEnter() {
-    this.selected = ''; // Reset saat halaman kembali aktif
-  }
-
-  setRating(value: number) {
-    this.rating = value;
-  }
-
-  submitRating() {
-    console.log('Rating diberikan:', this.rating);
-    this.navCtrl.navigateRoot('/dasboard-home');
-  }
-
-  dismiss() {
-    this.navCtrl.back();
-  }
-
-  selectMethod(method: string) {
-    this.selected = method;
-  }
-
-  async confirmMethod() {
-    if (this.selected) {
-      console.log('Metode dipilih:', this.selected);
-      this.navCtrl.navigateForward('/confirm-rating');
-    } else {
-      const alert = await this.alertController.create({
-        header: 'Input Rating',
-        message: 'Silakan input rating terlebih dahulu.',
-        buttons: [
-          {
-            text: 'Oke',
-            role: 'cancel',
-            cssClass: 'elegant-alert-button',
-          },
-        ],
-        cssClass: 'elegant-alert',
-        backdropDismiss: false,
-      });
-      await alert.present();
-    }
-  }
 }
